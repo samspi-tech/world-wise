29935dc7025cdfd4bfcf0833791b315eb370e5ad
samspi-tech <andrew.ryan.r.a@gmail.com>
2025-11-15 15:14:10 +0100

    add pages routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Products from './pages/Products';
+import Pricing from './pages/Pricing';
+import PageNotFound from './pages/PageNotFound';
+
 const App = () => {
-    return <div>Hello world!</div>;
+    return (
+        <Router>
+            <Routes>
+                <Route index element={<Homepage />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/pricing" element={<Pricing />} />
+                <Route path="/*" element={<PageNotFound />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
